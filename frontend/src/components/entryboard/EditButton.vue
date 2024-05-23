8af2101663587e47df57fac11996d41f7f687a6b
Matthew Hill <potatoman.mh@gmail.com>
2024-05-22 22:57:35 -0600

    Basic Entryboard UI w/ Dummy Data and manual edit funtionality.

---
+++ frontend/src/components/entryboard/EditButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="edit-button">
+    <button @click="$emit('editToggle')">Edit</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
