24855b1ea8fe5c6270f968fc5901838aedc315d4
VoodooWonderBot <awesome.robotman@gmail.com>
2024-06-05 20:20:35 -0600

    some prettying up

--- powerscore-entryboard/src/components/Entryboard.vue
+++ powerscore-entryboard/src/components/Entryboard.vue
@@ -5,16 +5,13 @@ import LockerRoomIdentifier from './LockerRoomIdentifier.vue';
 import TeamInfo from './TeamInfo.vue';
 import placeholderImage from "@/assets/img/hockeyClipart.jpg";
 
-// TODO: version of this page explicitly for the entryway (grab everything from server without edit button)
-// TODO: get editable version into main powerplay app
-
 interface Team {
   id: number
   name: string,
   image?: HTMLImageElement
 }
 
-function getPlaceholderImage(): HTMLImageElement {
+function getPlaceholderImage(): HTMLImageElement { // TODO: get images working
   var i = new Image();
   i.alt = "team logo";
   i.src = placeholderImage;
@@ -28,7 +25,7 @@ async function refreshTeams() {
   // TODO: Authentication
   // TODO: Do this on startup and/or periodically
   await axios
-    .get("http://localhost:3001/teams") // API endpoint here TODO: make this automatic somehow
+    .get("http://localhost:3001/teams") // API endpoint here TODO: make this externally configurable somehow
     .then(response => {
         console.debug(response)
         teams.value = response.data
